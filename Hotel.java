605fe280613518e4f4397248eb06bc79c79ae215
João Vitor <92281977+1455044@users.noreply.github.com>
2022-10-11 20:26:19 -0300

    Create Hotel.java

---
+++ Hotel.java
@@ -0,0 +1,14 @@
+public class Hotel {
+  private String categoria;
+
+  public Hotel (String categoria){
+    this.categoria=categoria;
+  }
+  public void setCategoria(String categoria){
+    this.categoria = categoria;
+  }
+
+  public String getCategoria(){
+    return this.categoria;
+  }
+}
